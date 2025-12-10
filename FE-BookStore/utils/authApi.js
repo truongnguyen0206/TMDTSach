adc9d17f30a2b1acb01dce8867f64534d5b394ca
Nhat_Truong <128435618+truongnguyen0206@users.noreply.github.com>
2025-12-10 14:42:12 +0700

    Update authApi.js

--- FE-BookStore/utils/authApi.js
+++ FE-BookStore/utils/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api" // ⚙️ chỉnh port nếu BE khác
+const baseURL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api";
 
 // Tự động gửi cookie khi cần
 axios.defaults.withCredentials = true
