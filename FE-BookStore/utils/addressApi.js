78f07a66949aebc0b6ee21b48597bde1c1345a11
truongnguyen0206 <nguyenvannhattruong2002@gmail.com>
2025-12-12 10:13:04 +0700

    Fix

--- FE-BookStore/utils/addressApi.js
+++ FE-BookStore/utils/addressApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000/api"
 axios.defaults.withCredentials = true;
 
 // 🏠 Thêm địa chỉ mới
